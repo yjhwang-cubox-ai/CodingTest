26d5f01c5e570444a87a634a795a0926aa1c7d49
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-07-11 17:50:27 +0900

    combination

--- combination.cpp
+++ combination.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int n = 5, k=3, a[5] = {1,2,3,4,5};
+
 void print(vector<int> b){
     for(int i:b) cout << i << " ";
     cout << "\n";
@@ -18,4 +19,17 @@ void combi(int start, vector<int> b){
         b.pop_back();
     }
     return;
+}
+
+int main() {
+    vector<int> b;
+    combi(-1, b);
+    return 0;
+}
+
+void combi(int start, vector<int>b){
+    if (b.size() == k){
+        print(b);
+        return;
+    }
 }
