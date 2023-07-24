ac78193c50a942f51138496f8f90ae4c74ac7dc8
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-07-24 12:40:34 +0900

    unique

--- unique.cpp
+++ unique.cpp
@@ -1,12 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//unique() 는 sort(), erase(unique()) 와 함께 쓰는 것이 좋다.
+// unique() 는 sort(), erase(unique()) 와 함께 쓰는 것이 좋다.
 
-int main() {
-    vector<int> s = {4,3,3,5,1,2,3};
-    sort(s.begin(), s.end()+1);
-    s.erase(unique(s.begin(), s.end()), s.end());
-    for (int i : s) cout << i << " ";
+void using_map(vector<int> v)
+{
+    map<int, int> mp;
+    // sort(v.begin(), v.end());
+    for (int i : v) {
+        if (mp[i])
+            continue;
+        else {
+            mp[i] = 1;
+        }
+    }
+    for (auto it : mp) {
+        cout << it.first << "   ";
+    }
+    cout << "\n";
+}
+
+void using_unique(vector<int> v)
+{
+    sort(v.begin(), v.end());
+    auto it = v.erase(unique(v.begin(), v.end()), v.end());
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    vector<int> s = { 4, 3, 3, 5, 1, 2, 3 };
+    using_map(s);
+    using_unique(s);
+
     return 0;
 }
